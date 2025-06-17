ad3d961d09acef61c71620babaad865ee0a82efd
Олег Усков <oleg81uskov@gmail.com>
2025-06-17 23:52:42 +0500

    fixses main.pu

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 # Импорт встроенной библиотеки для работы веб-сервера
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
@@ -11,18 +12,29 @@ class MyServer(BaseHTTPRequestHandler):
         Специальный класс, который отвечает за
         обработку входящих запросов от клиентов
     """
+    ROOT_DIR = os.path.dirname(__file__)
+
     def do_GET(self):
         """
         Метод для обработки входящих GET-запросов
         """
+        match self.path:
+            case "/main": path = self.ROOT_DIR + "/main.html"
+            case "/catalog": path = self.ROOT_DIR + "/catalog.html"
+            case "/category": path = self.ROOT_DIR + "/category.html"
+            case "/contacts": path = self.ROOT_DIR + "/contacts.html"
+            case _: path = self.ROOT_DIR + "/main.html"
+
         self.send_response(200) # Отправка кода ответа
-        self.send_header("Content-type", "application/json") # Отправка типа данных, который будет передаваться
+        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
         self.end_headers() # Завершение формирования заголовков ответа
-        self.wfile.write(bytes("{'message': 'OK'}", "utf-8")) # Тело ответа
+
+        with open(path, mode="r", encoding="UTF-8") as f:
+            self.wfile.write(bytes(f.read(), "utf-8"))  # Тело ответа
 
 if __name__ == "__main__":
     # Инициализация веб-сервера, который будет по заданным параметрах в сети
-    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    # принимать запросы и отправлять их на обработку специальному классу, описанному выше.
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
